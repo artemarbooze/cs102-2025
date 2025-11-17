1dcc5a3eb6ae7d331a866a7ce4525a71f20330f1
artemkorepanov <artkor07@mail.ru>
2025-11-17 19:15:01 +0300

    Исправлены ошибки

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,19 +9,28 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    keyword = keyword.upper()
-    key_length = len(keyword)
-    key_index = 0
 
-    for char in plaintext:
-        if char.isalpha():
-            base = ord("A") if char.isupper() else ord("a")
-            shift = ord(keyword[key_index % key_length]) - ord("A")
-            encrypted = (ord(char) - base + shift) % 26
-            ciphertext += chr(encrypted + base)
-            key_index += 1
-        else:
+    new_keyword = ""
+    while len(new_keyword) < len(plaintext):
+        new_keyword += keyword
+    keyword = new_keyword.lower()
+
+    for i in range(len(plaintext)):
+        char = plaintext[i]
+
+        if not char.isalpha():
             ciphertext += char
+            continue
+
+        shift = ord(keyword[i]) - 97
+        if char.islower():
+            x = ord(char)
+            new_x = (((x - 97) + shift) % 26) + 97
+            ciphertext += chr(new_x)
+        else:
+            x = ord(char)
+            new_x = (((x - 65) + shift) % 26) + 65
+            ciphertext += chr(new_x)
 
     return ciphertext
 
@@ -37,18 +46,27 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    keyword = keyword.upper()
-    key_length = len(keyword)
-    key_index = 0
 
-    for char in ciphertext:
-        if char.isalpha():
-            base = ord("A") if char.isupper() else ord("a")
-            shift = ord(keyword[key_index % key_length]) - ord("A")
-            decrypted = (ord(char) - base - shift) % 26
-            plaintext += chr(decrypted + base)
-            key_index += 1
-        else:
+    new_keyword = ""
+    while len(new_keyword) < len(ciphertext):
+        new_keyword += keyword
+    keyword = new_keyword.lower()
+
+    for i in range(len(ciphertext)):
+        char = ciphertext[i]
+
+        if not char.isalpha():
             plaintext += char
+            continue
+
+        shift = ord(keyword[i]) - 97
+        if char.islower():
+            x = ord(char)
+            new_x = (((x - 97) - shift) % 26) + 97
+            plaintext += chr(new_x)
+        else:
+            x = ord(char)
+            new_x = (((x - 65) - shift) % 26) + 65
+            plaintext += chr(new_x)
 
     return plaintext
