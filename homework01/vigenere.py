4b6e88f5c24125d59353683da36abcd414b5f133
artemkorepanov <artkor07@mail.ru>
2025-11-17 17:45:44 +0300

    Реализованы функции encrypt_vigenere() и decrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,7 +9,27 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    keyword_index = 0
+
+    for char in plaintext:
+        if char.isalpha():
+            if char.isupper():
+                base = ord("A")
+                key_char = keyword[keyword_index % len(keyword)]
+                shift = ord(key_char) - ord("A")
+            else:
+                base = ord("a")
+                key_char = keyword[keyword_index % len(keyword)].lower()
+                shift = ord(key_char) - ord("a")
+
+            encrypted_char = chr((ord(char) - base + shift) % 26 + base)
+            ciphertext += encrypted_char
+
+            keyword_index += 1
+        else:
+            ciphertext += char
+
     return ciphertext
 
 
@@ -24,5 +44,25 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    keyword_index = 0
+
+    for char in ciphertext:
+        if char.isalpha():
+            if char.isupper():
+                base = ord("A")
+                key_char = keyword[keyword_index % len(keyword)]
+                shift = ord(key_char) - ord("A")
+            else:
+                base = ord("a")
+                key_char = keyword[keyword_index % len(keyword)].lower()
+                shift = ord(key_char) - ord("a")
+
+            decrypted_char = chr((ord(char) - base - shift) % 26 + base)
+            plaintext += decrypted_char
+
+            keyword_index += 1
+        else:
+            plaintext += char
+
     return plaintext
